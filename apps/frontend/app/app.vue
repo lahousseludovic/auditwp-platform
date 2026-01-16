5f323b9a31ab2ca5d4ada05a9a5f7e31ea66faae
ludo <lahousseludovic@gmail.com>
2026-01-16 14:22:28 +0100

    Initial commit - frontend Nuxt 4

---
+++ apps/frontend/app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Bienvenue dans Nuxt 4 !</h1>
+</template>
+
+<script setup>
+console.log("Page chargée")
+</script>
---
+++ apps/frontend/pages/index.vue
@@ -0,0 +1,4 @@
+<!-- pages/index.vue -->
+<template>
+  <h1>Bienvenue dans Nuxt 4 !</h1>
+</template>
